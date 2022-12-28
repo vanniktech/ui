91f58b88595f352fd7679320576f7c0f622dccd4
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-12-28 14:04:59 +0100

    AndroidNightModeHandler: Fix in case an Application Context is passed into AndroidNightModeHandler.

--- sample-android/src/main/kotlin/com/vanniktech/ui/sample/android/UiMainActivity.kt
+++ sample-android/src/main/kotlin/com/vanniktech/ui/sample/android/UiMainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.vanniktech.ui.Color
 import com.vanniktech.ui.sample.android.databinding.ActivityMainBinding
+import com.vanniktech.ui.setBackgroundColor
 import com.vanniktech.ui.setTextColor
+import com.vanniktech.ui.themeWindow
+import com.vanniktech.ui.theming.night.NightModeBehavior
 
 class UiMainActivity : AppCompatActivity() {
   @SuppressLint("SetTextI18n")
@@ -17,5 +20,33 @@ class UiMainActivity : AppCompatActivity() {
 
     binding.textView.setTextColor(Color.UNTINTED)
     binding.textView.text = Color.UNTINTED.toString()
+
+    val androidNightModeHandler = (application as UiApplication).androidNightModeHandler
+    val isNightMode = androidNightModeHandler.isNightMode()
+    val backgroundColor = when (isNightMode) {
+      true -> Color.BLACK
+      else -> Color.WHITE
+    }
+    binding.root.setBackgroundColor(backgroundColor)
+
+    binding.nightMode.setTextColor(
+      when (isNightMode) {
+        true -> Color.WHITE
+        else -> Color.BLACK
+      },
+    )
+
+    window.themeWindow(
+      statusBarColor = backgroundColor,
+      navigationBarColor = backgroundColor,
+      lightNavigationBars = !isNightMode,
+      lightStatusBars = !isNightMode,
+    )
+
+    binding.nightMode.text = "Is night mode: $isNightMode"
+
+    binding.nightModeYes.setOnClickListener { androidNightModeHandler.updateBehavior(NightModeBehavior.YES) }
+    binding.nightModeNo.setOnClickListener { androidNightModeHandler.updateBehavior(NightModeBehavior.NO) }
+    binding.nightModeFollowSystem.setOnClickListener { androidNightModeHandler.updateBehavior(NightModeBehavior.FOLLOW_SYSTEM) }
   }
 }
--- ui/src/androidMain/kotlin/com/vanniktech/ui/theming/night/AndroidNightModeHandler.kt
+++ ui/src/androidMain/kotlin/com/vanniktech/ui/theming/night/AndroidNightModeHandler.kt
@@ -1,13 +1,44 @@
 package com.vanniktech.ui.theming.night
 
+import android.app.Activity
+import android.app.Application
+import android.app.Application.ActivityLifecycleCallbacks
 import android.content.Context
 import android.content.res.Configuration
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import com.vanniktech.ui.asActivityOrNull
+import java.lang.ref.WeakReference
 
 class AndroidNightModeHandler(
   private val context: Context,
 ) : NightModeHandler {
+  private var weak: WeakReference<Context>? = null
+
+  init {
+    val activity = context.asActivityOrNull()
+
+    if (activity != null) {
+      weak = WeakReference(activity)
+    } else if (context is Application) {
+      context.registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+        override fun onActivityPreCreated(activity: Activity, savedInstanceState: Bundle?) {
+          weak = WeakReference(activity)
+        }
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) = Unit
+        override fun onActivityStarted(activity: Activity) = Unit
+        override fun onActivityResumed(activity: Activity) = Unit
+        override fun onActivityPaused(activity: Activity) = Unit
+        override fun onActivityStopped(activity: Activity) = Unit
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+        override fun onActivityDestroyed(activity: Activity) = Unit
+      },
+      )
+    }
+  }
+
   override fun isNightMode(): Boolean {
+    val context = weak?.get() ?: context
     val isNightMode = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
     val isNotOptedOut = AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_NO
     return isNightMode && isNotOptedOut
