8acdc0e74df2be4cb47c994a659701033cf985f1
Niklas Baudy <niklas.baudy@vanniktech.de>
2023-05-01 12:03:26 +0100

    API: Support alpha in ColorPickerView & fix many bugs.

--- ui/src/androidMain/kotlin/com/vanniktech/ui/view/ColorHexInputFilter.kt
+++ ui/src/androidMain/kotlin/com/vanniktech/ui/view/ColorHexInputFilter.kt
@@ -4,22 +4,54 @@ import android.text.InputFilter
 import android.text.Spanned
 import com.vanniktech.ui.Color
 import com.vanniktech.ui.Color.Companion.HEX_DIGITS
+import com.vanniktech.ui.HEX_PREFIX
 
-internal object ColorHexInputFilter : InputFilter {
-  override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned?, dstart: Int, dend: Int): CharSequence? {
-    val new = source.subSequence(start, end).toString()
-    val value = dest.toString() + new
+internal class ColorHexInputFilter(
+  private val supportsAlpha: Boolean,
+) : InputFilter {
+  override fun filter(
+    source: CharSequence,
+    start: Int,
+    end: Int,
+    dest: Spanned,
+    dstart: Int,
+    dend: Int,
+  ): CharSequence? {
+    val dest1 = dest.subSequence(0, dstart).toString()
+    val new = source.subSequence(start, end)
+    val dest2 = dest.subSequence(dend, dest.length)
+    val value = dest1 + new + dest2
 
     // To support copy and paste. This is part I.
-    val color = Color.fromHexOrNull(new)
+    val color = inferColor(value, supportsAlpha)
     if (color != null) {
-      return color.hexString()
+      if (value.startsWith(HEX_PREFIX)) {
+        return value.removePrefix(HEX_PREFIX)
+      }
+
+      return null // Accept.
     }
 
-    if (value.all { HEX_DIGITS.contains(it) } && value.length <= 6) {
-      return null
+    val maxLength = when (supportsAlpha) {
+      true -> 8
+      else -> 6
+    }
+
+    if (value.all { HEX_DIGITS.contains(it) } && value.length <= maxLength) {
+      return null // Accept.
     }
 
     return "" // Reject.
   }
+}
+
+internal fun inferColor(string: String, supportsAlpha: Boolean): Color? {
+  val digits = setOf(
+    3,
+    4.takeIf { supportsAlpha },
+    6,
+    8.takeIf { supportsAlpha },
+  )
+  val cleanedString = string.removePrefix(HEX_PREFIX)
+  return Color.fromHexOrNull(cleanedString).takeIf { digits.contains(cleanedString.length) }
 }
