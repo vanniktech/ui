be5c3c9314a90c0096d0a3d3349474c36b981f00
Niklas Baudy <niklas.baudy@vanniktech.de>
2023-08-10 15:40:22 +0100

    API: Provide DurationParceler & TypeParceler annotation. (#74)

--- ui/src/androidMain/kotlin/com/vanniktech/ui/commonParcelableAndroidApi.kt
+++ ui/src/androidMain/kotlin/com/vanniktech/ui/commonParcelableAndroidApi.kt
@@ -1,5 +1,29 @@
 package com.vanniktech.ui
 
+import android.os.Parcel
+import kotlinx.parcelize.Parceler
+import kotlinx.parcelize.TypeParceler
+import kotlin.time.Duration
+
 actual typealias Parcelable = android.os.Parcelable
 actual typealias Parcelize = kotlinx.parcelize.Parcelize
 actual typealias IgnoredOnParcel = kotlinx.parcelize.IgnoredOnParcel
+actual typealias TypeParceler<T, P> = TypeParceler<T, P>
+actual typealias Parceler<T> = Parceler<T>
+
+actual object DurationParceler : Parceler<Duration?> {
+  override fun create(parcel: Parcel): Duration? {
+    return when (parcel.readInt() == 1) {
+      true -> parcel.readString()?.let(Duration::parseIsoStringOrNull)
+      else -> null
+    }
+  }
+
+  override fun Duration?.write(parcel: Parcel, flags: Int) {
+    parcel.writeInt(if (this != null) 1 else 0)
+
+    if (this != null) {
+      parcel.writeString(this.toIsoString())
+    }
+  }
+}
--- ui/src/jvmMain/kotlin/com/vanniktech/ui/commonParcelableJvmApi.kt
+++ ui/src/jvmMain/kotlin/com/vanniktech/ui/commonParcelableJvmApi.kt
@@ -1,3 +1,8 @@
 package com.vanniktech.ui
 
+import kotlin.time.Duration
+
 actual interface Parcelable
+actual interface Parceler<T>
+
+actual object DurationParceler : Parceler<Duration?>
