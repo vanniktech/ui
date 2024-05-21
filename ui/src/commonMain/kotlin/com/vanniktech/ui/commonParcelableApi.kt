96b230912b5071e4db90e2eeebe6fbf65bbd4c34
Niklas Baudy <niklas.baudy@vanniktech.de>
2024-05-21 20:33:51 +0800

    Breaking: Hide Parcelable types, no longer works with K2.

--- ui/src/commonMain/kotlin/com/vanniktech/ui/commonParcelableApi.kt
+++ ui/src/commonMain/kotlin/com/vanniktech/ui/commonParcelableApi.kt
@@ -2,8 +2,6 @@
 
 package com.vanniktech.ui
 
-import kotlin.time.Duration
-
 expect interface Parcelable
 
 @OptIn(ExperimentalMultiplatform::class)
@@ -11,20 +9,3 @@ expect interface Parcelable
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
 expect annotation class Parcelize()
-
-@OptIn(ExperimentalMultiplatform::class)
-@OptionalExpectation
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.SOURCE)
-expect annotation class IgnoredOnParcel
-
-@OptIn(ExperimentalMultiplatform::class)
-@OptionalExpectation
-@Retention(AnnotationRetention.SOURCE)
-@Repeatable
-@Target(AnnotationTarget.CLASS, AnnotationTarget.PROPERTY)
-expect annotation class TypeParceler<T, P : Parceler<in T>>
-
-expect interface Parceler<T>
-
-expect object DurationParceler : Parceler<Duration?>
